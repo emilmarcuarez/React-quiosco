5a2fc507dc304c174f13badbd02857d57208a258
emilmarcuarez <emilmarpatricia@gmail.com>
2024-11-09 23:37:05 -0400

    Colocando las categorias en context

--- react-quiosco/src/context/QuioscoProvider.jsx
+++ react-quiosco/src/context/QuioscoProvider.jsx
@@ -1,16 +1,22 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
+import { categorias as categoriasDB} from "../data/categorias"
 import PropTypes from 'prop-types';
+
 
 const QuioscoContext = createContext();
 
 
 const QuioscoProvider = ({children}) => {
 
-    const autenticado = true;
+    // primer valor: lo que voy a usar (nombre del state) y lo otro es para modificarlo
+    const [categorias, setCategorias ] =useState(categoriasDB) ;
+
+
+   
     return (
         <QuioscoContext.Provider 
         value={{
-            autenticado
+            categorias
         }}
 
         >{children}</QuioscoContext.Provider>
